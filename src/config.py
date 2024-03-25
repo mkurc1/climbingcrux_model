8bc35abd507b0ec5dfbb33a0b39088c8c89b5acb
Michał Kurcewicz <mkurc1@gmail.com>
2024-03-25 20:37:48 +0100

    Extract config

--- src/config.py
+++ src/config.py
@@ -1,5 +1,11 @@
+import os
+from pathlib import Path
 import cv2
 
 
+BASE_DIR = Path(__file__).resolve().parent.parent
+
 MARKER_PERIMETER_IN_CM = 4 * 7
 MARKER_ARUCO_DICT = cv2.aruco.DICT_5X5_50
+
+YOLO_CONFIG_PATH = os.path.join(BASE_DIR, 'config.yaml')
