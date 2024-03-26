be13ce83befe9175c7cfc2bb9b96d73bcc4f4cce
Michał Kurcewicz <mkurc1@gmail.com>
2024-03-26 21:13:34 +0100

    Refactor config

--- src/config.py
+++ src/config.py
@@ -9,3 +9,13 @@ MARKER_PERIMETER_IN_CM = 4 * 7
 MARKER_ARUCO_DICT = cv2.aruco.DICT_5X5_50
 
 YOLO_CONFIG_PATH = os.path.join(BASE_DIR, 'config.yaml')
+YOLO_BASE_MODEL = 'yolov9c.pt'
+YOLO_MODEL_PATH = os.path.join(BASE_DIR, 'notebooks', 'runs', 'detect', 'train3', 'weights', 'best.pt')
+
+MARKER_BBOX_COLOR = (0, 255, 0)
+TXT_COLOR = (0, 0, 0)
+TXT_BG_COLOR = (255, 255, 255)
+BBOX_COLOR = (0, 255, 0)
+BBOX_CENTER_COLOR = (0, 0, 255)
+
+LINE_WIDTH = 2
