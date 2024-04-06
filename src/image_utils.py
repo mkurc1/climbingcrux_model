b488ec5d863078dc34d7b0365cffc3ac54ddc63c
Michał Kurcewicz <mkurc1@gmail.com>
2024-04-06 20:27:02 +0200

    Add second starting step

--- src/image_utils.py
+++ src/image_utils.py
@@ -4,7 +4,12 @@ from PIL import Image
 from IPython.display import display
 
 
-def draw_bboxes(img: cv2.typing.MatLike, bboxes: np.ndarray, classes: np.ndarray, class_names: dict, centers: np.ndarray, bbox_color: cv2.typing.Scalar, bbox_center_color: cv2.typing.Scalar, line_width: int) -> None:
+def draw_bboxes(img: cv2.typing.MatLike, bboxes: np.ndarray, classes: np.ndarray, class_names: dict,
+                centers: np.ndarray, bbox_color: cv2.typing.Scalar, bbox_center_color: cv2.typing.Scalar,
+                line_width: int, override: bool = True) -> cv2.typing.MatLike:
+    if not override:
+        img = img.copy()
+
     for cls, bbox, center in zip(classes, bboxes, centers):
         x1, y1, x2, y2 = bbox
 
@@ -13,6 +18,7 @@ def draw_bboxes(img: cv2.typing.MatLike, bboxes: np.ndarray, classes: np.ndarray
 
         result_name = class_names[cls]
         cv2.putText(img, result_name, (x1, y1 - 5), cv2.FONT_HERSHEY_PLAIN, 1, bbox_color, line_width)
+    return img
 
 
 def display_image(img: cv2.typing.MatLike) -> None:
