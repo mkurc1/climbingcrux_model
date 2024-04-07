bcc6831754ce8cdba93027d3d86e650b5bfb94f8
Michał Kurcewicz <mkurc1@gmail.com>
2024-04-07 22:16:06 +0200

    Refactor detected object

--- src/image_utils.py
+++ src/image_utils.py
@@ -1,22 +1,21 @@
 import cv2
-import numpy as np
 from PIL import Image
 from IPython.display import display
+from src.detected_object import DetectedObject
 
 
-def draw_bboxes(img: cv2.typing.MatLike, bboxes: np.ndarray, classes: np.ndarray, class_names: dict,
-                centers: np.ndarray, bbox_color: cv2.typing.Scalar, bbox_center_color: cv2.typing.Scalar,
-                line_width: int, override: bool = True) -> cv2.typing.MatLike:
+def draw_bboxes(img: cv2.typing.MatLike, detected_objects: [DetectedObject], bbox_color: cv2.typing.Scalar,
+                bbox_center_color: cv2.typing.Scalar, line_width: int, override: bool = True) -> cv2.typing.MatLike:
     if not override:
         img = img.copy()
 
-    for cls, bbox, center in zip(classes, bboxes, centers):
-        x1, y1, x2, y2 = bbox
+    for detected_object in detected_objects:
+        x1, y1, x2, y2 = detected_object.bbox
 
         cv2.rectangle(img, (x1, y1), (x2, y2), bbox_color, line_width)
-        cv2.circle(img, (int(center[0]), int(center[1])), 5, bbox_center_color, -1)
+        cv2.circle(img, detected_object.get_center(), 5, bbox_center_color, -1)
 
-        result_name = class_names[cls]
+        result_name = detected_object.class_name
         cv2.putText(img, result_name, (x1, y1 - 5), cv2.FONT_HERSHEY_PLAIN, 1, bbox_color, line_width)
     return img
 
