48fc7d0bab3b820e0a5a29e5397bf55a2819f78c
Michał Kurcewicz <mkurc1@gmail.com>
2024-04-22 16:58:17 +0200

    Update stating position

--- src/model/body_proportion.py
+++ src/model/body_proportion.py
@@ -1,9 +1,12 @@
+# This file contains the BodyProportion class which is used to calculate the body proportions of a person based on their height.
+# This class based on data from the website: https://hpc.anatomy4sculptors.com/
+
 class BodyProportion:
     def __init__(self, height: int):
         self.height = height
         self.head = self.height / 8
         self.neck = self.head / 3
-        self.shoulder = self.head * 2
+        self.shoulder = self.head
         self.trunk = self.head * 2.6
         self.arm = self.head * 3.5
         self.leg = self.head * 4
