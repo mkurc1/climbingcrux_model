76a0fdca0657a8391d8c729d568ab73f38d6950b
Michał Kurcewicz <mkurc1@gmail.com>
2024-05-01 20:54:56 +0200

    Refactor climber start position

--- src/climber_start_position.py
+++ src/climber_start_position.py
@@ -15,10 +15,10 @@ class ClimberStartPosition:
         self.img_height = img_height
         self.marker = marker
         self.detected_objects = detected_objects
+        self.climber = Climber()
 
     def prepare(self, climber_height_in_cm: int,
                 starting_steps_max_distance_from_ground_in_cm: int) -> Climber:
-        climber = Climber()
         climber_height_in_px = self.marker.convert_cm_to_pixel(climber_height_in_cm)
         body_proportion = BodyProportion(climber_height_in_px)
 
@@ -57,11 +57,11 @@ class ClimberStartPosition:
         position_between_starting_steps = (starting_step_1.get_center()[0] + starting_step_2.get_center()[0]) / 2
 
         top_head_point = (
-        int(position_between_starting_steps), int(lower_starting_step.get_center()[1] - body_proportion.height))
+            int(position_between_starting_steps), int(lower_starting_step.get_center()[1] - body_proportion.height))
         bottom_head_point = (int(position_between_starting_steps),
                              int(lower_starting_step.get_center()[1] - body_proportion.height + body_proportion.head))
 
-        climber.head = BodyPart(
+        self.climber.head = BodyPart(
             start=top_head_point,
             end=bottom_head_point,
             color=Color(255, 255, 0),  # yellow color
@@ -71,7 +71,7 @@ class ClimberStartPosition:
         top_neck_point = (top_head_point[0], int(bottom_head_point[1]))
         bottom_neck_point = (bottom_head_point[0], int(bottom_head_point[1] + body_proportion.neck))
 
-        climber.neck = BodyPart(
+        self.climber.neck = BodyPart(
             start=top_neck_point,
             end=bottom_neck_point,
             color=Color(0, 125, 255),  # orange color
@@ -81,7 +81,7 @@ class ClimberStartPosition:
         top_trunk_point = (top_head_point[0], int(bottom_neck_point[1]))
         bottom_trunk_point = (bottom_head_point[0], int(bottom_neck_point[1] + body_proportion.trunk))
 
-        climber.trunk = BodyPart(
+        self.climber.trunk = BodyPart(
             start=top_trunk_point,
             end=bottom_trunk_point,
             color=Color.green(),  # green color
@@ -91,7 +91,7 @@ class ClimberStartPosition:
         start_left_shoulder_point = (bottom_neck_point[0] - int(body_proportion.shoulder), bottom_neck_point[1])
         end_left_shoulder_point = (bottom_neck_point[0], bottom_neck_point[1])
 
-        climber.left_shoulder = BodyPart(
+        self.climber.left_shoulder = BodyPart(
             start=start_left_shoulder_point,
             end=end_left_shoulder_point,
             color=Color.blue(),
@@ -101,7 +101,7 @@ class ClimberStartPosition:
         start_right_shoulder_point = (bottom_neck_point[0], bottom_neck_point[1])
         end_right_shoulder_point = (bottom_neck_point[0] + int(body_proportion.shoulder), bottom_neck_point[1])
 
-        climber.right_shoulder = BodyPart(
+        self.climber.right_shoulder = BodyPart(
             start=start_right_shoulder_point,
             end=end_right_shoulder_point,
             color=Color.blue(),
@@ -118,7 +118,7 @@ class ClimberStartPosition:
         start_left_leg_point = (bottom_trunk_point[0], bottom_trunk_point[1])
         end_left_leg_point = starting_left_step.get_center()
 
-        climber.left_leg = BodyPart(
+        self.climber.left_leg = BodyPart(
             start=start_left_leg_point,
             end=end_left_leg_point,
             color=Color.blue(),
@@ -128,53 +128,58 @@ class ClimberStartPosition:
         start_right_leg_point = (bottom_trunk_point[0], bottom_trunk_point[1])
         end_right_leg_point = starting_right_step.get_center()
 
-        climber.right_leg = BodyPart(
+        self.climber.right_leg = BodyPart(
             start=start_right_leg_point,
             end=end_right_leg_point,
             color=Color.blue(),
             thickness=10
         )
 
+        self.climber.left_hand = self.__find_hold_for_left_hand(body_proportion, position_between_starting_steps)
+        self.climber.right_hand = self.__find_hold_for_right_hand(body_proportion, position_between_starting_steps)
+
+        return self.climber
+
+    def __find_hold_for_left_hand(self, body_proportion: BodyProportion,
+                                  position_between_starting_steps: tuple[int, int]) -> BodyPart:
         # get holds available for left hand
-        holds_available_for_left_hand = objects_detector.get_objects_around_point(
+        holds = objects_detector.get_objects_around_point(
             detected_objects=self.detected_objects,
-            point=climber.left_shoulder.start,
+            point=self.climber.left_shoulder.start,
             radius=int(body_proportion.arm)
         )
 
         # exclude holds that are on the right side of the position_between_starting_steps
-        holds_available_for_left_hand = [hold for hold in holds_available_for_left_hand if
-                                         hold.get_center()[0] < position_between_starting_steps]
+        holds = [hold for hold in holds if hold.get_center()[0] < position_between_starting_steps]
 
+        random_left_hand_hold_id = np.random.choice(len(holds), 1, replace=False)[0]
+        random_left_hand_hold = holds[random_left_hand_hold_id]
+
+        return BodyPart(
+            start=self.climber.left_shoulder.start,
+            end=random_left_hand_hold.get_center(),
+            color=Color.red(),
+            thickness=10
+        )
+
+    def __find_hold_for_right_hand(self, body_proportion: BodyProportion,
+                                   position_between_starting_steps: tuple[int, int]) -> BodyPart:
         # get holds available for right hand
-        holds_available_for_right_hand = objects_detector.get_objects_around_point(
+        holds = objects_detector.get_objects_around_point(
             detected_objects=self.detected_objects,
-            point=climber.right_shoulder.end,
+            point=self.climber.right_shoulder.end,
             radius=int(body_proportion.arm)
         )
 
         # exclude holds that are on the left side of the position_between_starting_steps
-        holds_available_for_right_hand = [hold for hold in holds_available_for_right_hand if
-                                          hold.get_center()[0] > position_between_starting_steps]
+        holds = [hold for hold in holds if hold.get_center()[0] > position_between_starting_steps]
 
-        random_left_hand_hold_id = np.random.choice(len(holds_available_for_left_hand), 1, replace=False)[0]
-        random_left_hand_hold = holds_available_for_left_hand[random_left_hand_hold_id]
+        random_right_hand_hold_id = np.random.choice(len(holds), 1, replace=False)[0]
+        random_right_hand_hold = holds[random_right_hand_hold_id]
 
-        climber.left_hand = BodyPart(
-            start=random_left_hand_hold.get_center(),
-            end=climber.left_shoulder.start,
-            color=Color.red(),
-            thickness=10
-        )
-
-        random_right_hand_hold_id = np.random.choice(len(holds_available_for_right_hand), 1, replace=False)[0]
-        random_right_hand_hold = holds_available_for_right_hand[random_right_hand_hold_id]
-
-        climber.right_hand = BodyPart(
-            start=climber.right_shoulder.end,
+        return BodyPart(
+            start=self.climber.right_shoulder.end,
             end=random_right_hand_hold.get_center(),
             color=Color.red(),
             thickness=10
         )
-
-        return climber
