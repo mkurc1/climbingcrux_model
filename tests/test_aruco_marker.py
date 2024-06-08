022c6b15a3bf15a169e87854e8e8c294f472f7f9
Michał Kurcewicz <mkurc1@gmail.com>
2024-06-08 21:31:35 +0200

    Add tests for detect marker

--- tests/test_aruco_marker.py
+++ tests/test_aruco_marker.py
@@ -22,3 +22,39 @@ def test_exception_when_no_marker_detected() -> None:
     # when and then
     with pytest.raises(ValueError):
         ArucoMarker(config.MARKER_ARUCO_DICT, img, config.MARKER_PERIMETER_IN_CM)
+
+
+def test_get_width_in_cm() -> None:
+    # given
+    img = cv2.imread('./files/aruco_marker.jpeg')
+    aruco_marker = ArucoMarker(config.MARKER_ARUCO_DICT, img, config.MARKER_PERIMETER_IN_CM)
+
+    # when
+    width_in_cm = aruco_marker.get_width_in_cm()
+
+    # then
+    assert round(width_in_cm) == 7
+
+
+def test_get_height_in_cm() -> None:
+    # given
+    img = cv2.imread('./files/aruco_marker.jpeg')
+    aruco_marker = ArucoMarker(config.MARKER_ARUCO_DICT, img, config.MARKER_PERIMETER_IN_CM)
+
+    # when
+    height_in_cm = aruco_marker.get_height_in_cm()
+
+    # then
+    assert round(height_in_cm) == 7
+
+
+def test_get_pixels_per_centimeter() -> None:
+    # given
+    img = cv2.imread('./files/aruco_marker.jpeg')
+    aruco_marker = ArucoMarker(config.MARKER_ARUCO_DICT, img, config.MARKER_PERIMETER_IN_CM)
+
+    # when
+    pixels_per_centimeter = aruco_marker.get_pixels_per_centimeter()
+
+    # then
+    assert round(pixels_per_centimeter) == 79
